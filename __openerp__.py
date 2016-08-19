e63437ee8c392a38205273f789d72a193a1b5463
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-19 15:04:10 +0200

    update version to 9

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
    'name': "Mass invoice mailing",
-    'version': '1.0',
+    'version': '9.0.1.0',
     'depends': ['account'],
     'author': "Bernard DELHEZ, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
